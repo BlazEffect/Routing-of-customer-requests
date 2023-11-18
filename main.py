83f0a50482657df738178a63e4ec01eb86a03441
Djok161 <42178475+Djok161@users.noreply.github.com>
2023-11-18 17:53:12 +0300

    Моделька+эндпоинт

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
 from routers.VectorRouter import model_router
-from routers.token import token_router
 
 app = FastAPI()
 
 app.include_router(
 	router= model_router,
 	prefix= '/api/model',
-	tags=['Распознование текста']
-)
-
-app.include_router(
-	router= token_router,
-	prefix= '/api',
-	tags=['Получение JWT токена']
+	tags=['text']
 )
