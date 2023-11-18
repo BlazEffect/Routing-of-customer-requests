d0316ffc83587cd1f4f1f9f9d78b40f730d97b10
BlazEffect <kamalinigor@gmail.com>
2023-11-18 04:20:41 +0300

    Designed the login page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
-import '../src/assets/css/Chat.css';
 import '../src/assets/css/App.css';
 import '../src/assets/css/Main.css';
+import '../src/assets/css/Chat.css';
+import '../src/assets/css/Login.css';
 import router from './router/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,39 @@
+import Button from '../components/Button.jsx';
+import { useState } from 'react';
+
 export default function Login() {
+  const [loginInput, setLoginInput] = useState();
+  const [password, setPassword] = useState();
+
   return (
-    <div>
-      Login
+    <div className="auth">
+      <h3 className="auth__greeter-text">Вход в панель для просмотра тикетов</h3>
+
+      <form action="" className="auth-form">
+        <input
+          type="text"
+          id="userLogin"
+          placeholder="Логин"
+          className="auth-form__input"
+          name="userLogin"
+          value={loginInput}
+          onChange={(e) => setLoginInput(e.target.value)}
+        />
+
+        <input
+          type="password"
+          id="userPassword"
+          placeholder="Пароль"
+          className="auth-form__input"
+          name="userPassword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <Button className="auth__button">
+          Войти
+        </Button>
+      </form>
     </div>
   )
 }
