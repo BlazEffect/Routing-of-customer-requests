39b9eb16656c30bb1e0cfd34cfae93b10c7ee80b
BlazEffect <kamalinigor@gmail.com>
2023-11-18 03:50:27 +0300

    Changed the button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,4 @@
 export default function Button({ handleClick, children, className }) {
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ');
-  }
-
   return (
     <div
       onClick={handleClick}
