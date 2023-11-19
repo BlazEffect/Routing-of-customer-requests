a02f7b3b19cf49f898bcdbf14eb897ec59f4a779
BlazEffect <kamalinigor@gmail.com>
2023-11-19 11:24:06 +0300

    wip

--- src/components/MessageSend.jsx
+++ src/components/MessageSend.jsx
@@ -10,8 +10,10 @@ export default function MessageSend({ sendTo, messages, setMessage, setConsultan
 
     const currentDate = new Date();
 
-    const newUserMessageId = Object.keys(messages).length + 1;
-    const newUserMessage = {
+    const words = userInput.split(' ');
+
+    let newUserMessageId = Object.keys(messages).length + 1;
+    let newUserMessage = {
       id: newUserMessageId,
       senderName: 'Пользователь',
       sendTo: sendTo,
@@ -19,45 +21,60 @@ export default function MessageSend({ sendTo, messages, setMessage, setConsultan
       messageText: userInput
     };
 
-    const newUserMessages = { ...messages, [newUserMessageId]: newUserMessage };
+    let newUserMessages = { ...messages, [newUserMessageId]: newUserMessage };
 
     setMessage(newUserMessages);
 
-    axios.post('/api/model/recognition/?text=' + userInput).then((data) => {
-      let importance = '';
+    if (words.length > 5) {
+      axios.post('/api/model/recognition/?text=' + userInput).then((data) => {
+        let importance = '';
 
-      switch (data.data.importance) {
-        case 'standart_priority':
-          importance = 'Обычный приоритет';
-          break;
-        case 'medium_priority':
-          importance = 'Средний приоритет';
-          break;
-        case 'high_priority':
-          importance = 'Высокий приоритет';
-          break;
-      }
+        switch (data.data.importance) {
+          case 'standart_priority':
+            importance = 'Обычный приоритет';
+            break;
+          case 'medium_priority':
+            importance = 'Средний приоритет';
+            break;
+          case 'high_priority':
+            importance = 'Высокий приоритет';
+            break;
+        }
 
-      const ticketMessage = 'Мы обработали ваш запрос: Ваш менеджер: ' + data.data.sent_to_employee + ' Приоиртет' +
-        ' в очереди: ' + importance + ' Тема запроса: ' + ticketNames[data.data.request];
+        const ticketMessage = 'Мы обработали ваш запрос: Ваш менеджер: ' + data.data.sent_to_employee + ' Приоиртет' +
+          ' в очереди: ' + importance + ' Тема запроса: ' + ticketNames[data.data.request];
 
-      const newAdminMessageId = Object.keys(newUserMessages).length + 1;
-      const newAdminMessage = {
-        id: newAdminMessageId,
-        senderName: 'Пользователь',
+        const newAdminMessageId = Object.keys(newUserMessages).length + 1;
+        const newAdminMessage = {
+          id: newAdminMessageId,
+          senderName: 'Пользователь',
+          sendTo: 'User',
+          createdAt: currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+          messageText: ticketMessage
+        };
+
+        const newAdminMessages = { ...newUserMessages, [newAdminMessageId]: newAdminMessage };
+
+        setMessage(newAdminMessages);
+
+        setConsultantName(data.data.sent_to_employee);
+      });
+
+      setUserInput('');
+    } else {
+      newUserMessageId = Object.keys(newUserMessages).length + 1;
+      newUserMessage = {
+        id: newUserMessageId,
+        senderName: 'Админ',
         sendTo: 'User',
         createdAt: currentDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
-        messageText: ticketMessage
+        messageText: 'Слишком маленькое сообщение'
       };
 
-      const newAdminMessages = { ...newUserMessages, [newAdminMessageId]: newAdminMessage };
+      newUserMessages = { ...newUserMessages, [newUserMessageId]: newUserMessage };
 
-      setMessage(newAdminMessages);
-
-      setConsultantName(data.data.sent_to_employee);
-    });
-
-    setUserInput('');
+      setMessage(newUserMessages);
+    }
   }
 
   return (
