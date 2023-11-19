a302956c3c74b72da6d0676fc7a2b766d3b76afe
Djok161 <42178475+Djok161@users.noreply.github.com>
2023-11-19 03:23:36 +0300

    Помогите

--- routers/VectorRouter.py
+++ routers/VectorRouter.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from models.database import get_db
 from vectors.models import classify
 from datetime import datetime
 from sqlalchemy.orm import Session
@@ -10,7 +12,6 @@ model_router = APIRouter()
 def create_ticket(db: Session, name: str, prior: str, message: str, send_to: str):
 	try:
 		db.begin()
-
 		new_ticket = Ticket(name=name, prior=prior)
 		db.add(new_ticket)
 		db.flush()
@@ -18,16 +19,31 @@ def create_ticket(db: Session, name: str, prior: str, message: str, send_to: str
 										   sendTo=send_to)
 		db.add(new_ticket_message)
 		db.flush()
-
 		db.commit()
-
 		return new_ticket, new_ticket_message
 	except Exception as e:
-		# Откат транзакции в случае ошибки
 		db.rollback()
 		raise e
 
 
 @model_router.post('/recognition')
-def model_req(text: str):
-	return classify(text)
+def model_req(text: str, db: Session = Depends(get_db)):
+	classification_result = classify(text)
+
+	request_result = classification_result.get("request")
+	importance_result = classification_result.get("importance")
+
+	new_ticket, new_ticket_message = create_ticket(
+		db=db,
+		name="Recognition Ticket",
+		prior=str(importance_result),
+		message=text,
+		send_to="Admin"
+	)
+
+	return {
+		"request": request_result,
+		"importance": importance_result,
+		"ticket_id": new_ticket.id,
+		"ticket_message_id": new_ticket_message.id
+	}
