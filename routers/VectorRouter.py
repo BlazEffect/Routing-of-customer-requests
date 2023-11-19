f7ae7abda435b12903cd0f5d55f84407247b7c69
Djok161 <42178475+Djok161@users.noreply.github.com>
2023-11-19 04:23:13 +0300

    Помогите

--- routers/VectorRouter.py
+++ routers/VectorRouter.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-
+import pandas as pd
 from models.database import get_db
 from vectors.models import classify
 from datetime import datetime
@@ -9,41 +9,59 @@ from models.core import Ticket, TicketMessage
 model_router = APIRouter()
 
 
-def create_ticket(db: Session, name: str, prior: str, message: str, send_to: str):
-	try:
-		db.begin()
-		new_ticket = Ticket(name=name, prior=prior)
-		db.add(new_ticket)
-		db.flush()
-		new_ticket_message = TicketMessage(ticket=new_ticket.id, message=message, createdAt=datetime.now(),
-										   sendTo=send_to)
-		db.add(new_ticket_message)
-		db.flush()
-		db.commit()
-		return new_ticket, new_ticket_message
-	except Exception as e:
-		db.rollback()
-		raise e
+
+
+def create_ticket_with_tracking(db: Session, name: str, prior: str, message: str, send_to: str):
+    try:
+        db.begin()
+        new_ticket = Ticket(name=name, prior=prior)
+        db.add(new_ticket)
+        db.flush()
+        new_ticket_message = TicketMessage(ticket=new_ticket.id, message=message, createdAt=datetime.now(),
+                                           sendTo=send_to)
+        db.add(new_ticket_message)
+        db.flush()
+        db.commit()
+        return new_ticket, new_ticket_message
+    except Exception as e:
+        db.rollback()
+        raise e
 
 
 @model_router.post('/recognition')
 def model_req(text: str, db: Session = Depends(get_db)):
-	classification_result = classify(text)
+    classification_result = classify(text)
 
-	request_result = classification_result.get("request")
-	importance_result = classification_result.get("importance")
+    request_result = classification_result.get("request")
+    importance_result = classification_result.get("importance")
+    df = pd.read_csv('vectors/employers.csv')
 
-	new_ticket, new_ticket_message = create_ticket(
-		db=db,
-		name="Recognition Ticket",
-		prior=str(importance_result),
-		message=text,
-		send_to="Admin"
-	)
+    available_employees = df[df[f'{request_result}'] == 1]
 
-	return {
-		"request": request_result,
-		"importance": importance_result,
-		"ticket_id": new_ticket.id,
-		"ticket_message_id": new_ticket_message.id
-	}
+    if available_employees.empty:
+        return {"error": "No employees"}
+
+    # Находим работника с наименьшим количеством задач
+    employee_with_least_tasks = available_employees.loc[available_employees['task_counter'].idxmin()]
+    send_to_employee = employee_with_least_tasks['employee']
+
+    new_ticket, new_ticket_message = create_ticket_with_tracking(
+        db=db,
+        name="Recognition Ticket",
+        prior=str(importance_result),
+        message=text,
+        send_to=send_to_employee
+    )
+
+    # Увеличиваем счетчик задач для выбранного работника
+    available_employees.loc[employee_with_least_tasks.name, 'task_counter'] += 1
+    available_employees.to_csv('vectors/employers.csv', index=False)
+    df = pd.read_csv('vectors/employers.csv')
+
+    return {
+        "request": request_result,
+        "importance": importance_result,
+        "ticket_id": new_ticket.id,
+        "ticket_message_id": new_ticket_message.id,
+        "sent_to_employee": send_to_employee,
+    }
