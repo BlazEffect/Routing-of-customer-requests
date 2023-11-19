4087717c7edafa66be65249db266b74ff881d487
BlazEffect <kamalinigor@gmail.com>
2023-11-19 03:00:18 +0300

    Added env file and avoided cors

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from routers.VectorRouter import model_router
 from routers.token import token_router
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 app = FastAPI()
+
+origins = [
+    os.getenv("CURRENT_HOST"),
+    os.getenv("VITE_HOST")
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
 	router= model_router,
